1e511b5fed10d204517b981ea5bd30b2e9357b38
awabqais <awab.qais@gmail.com>
2025-09-20 09:04:09 +0300

    Commit#2
    In the NV on line 7,8,9,and 10,declare jframe , cardlayout, jpanel,and jmenubar

---
+++ src/main/java/org/example/WorkingWithMenusPanels.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class WorkingWithMenusPanels {
+    JFrame frame;
+    CardLayout cardLayout;
+    JPanel cardPanel;
+    JMenuBar menuBar;
+}
