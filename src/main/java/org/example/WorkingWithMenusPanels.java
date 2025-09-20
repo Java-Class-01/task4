5cda25329c79c080a5c07def14c456f879b1ad63
awabqais <awab.qais@gmail.com>
2025-09-20 09:14:43 +0300

    Commit#1
    On the NV on line 12 ,and 13,create a constructor hold method called createMainFrame then on line 16 we create a method called createMainFrame then on line 18,20,and 21 we set the decoration ,create the frame and give size

--- src/main/java/org/example/WorkingWithMenusPanels.java
+++ src/main/java/org/example/WorkingWithMenusPanels.java
@@ -8,4 +8,19 @@ public class WorkingWithMenusPanels {
     CardLayout cardLayout;
     JPanel cardPanel;
     JMenuBar menuBar;
+
+    public WorkingWithMenusPanels() {
+        this.createMainFrame();
+    }
+
+    public JFrame createMainFrame() {
+        // set look and feel
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        frame = new JFrame("Swing Application with Menu and Panels");
+        frame.setSize(600, 400);
+
+        return frame;
+    }
+
 }
